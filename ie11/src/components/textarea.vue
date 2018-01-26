d61216582e08802821ee1beec56fd0119dadb70e
吕书心 <lvsx@sdc.icbc.com.cn>
2018-01-26 13:59:56 +0800

    create a reproduction of vue issue #7480.

---
+++ ie11/src/components/textarea.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    }
+  },
+  render(h) {
+    return h("textarea", {
+      on: {
+        input: e => {
+          this.$emit("update:value", e.target.value);
+        }
+      }
+    }, [this.value]);
+  }
+};
+</script>
